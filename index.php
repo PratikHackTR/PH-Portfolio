19dafb6400a6d16a2da3dc9e89a32f8cb05f3471
PratikHackTR <burakdemir_karakartal@hotmail.com>
2024-02-11 04:12:56 +0300

    Portfolio website

---
+++ index.php
@@ -0,0 +1,351 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Portfolio,Burak Demir , PratikHack , BurakDemirCV,PratikHackCV,PratikHack01,BurakDemir01,BurakPH">
+    <meta name="author" content="Devcrud">
+    <title>PHPortfolio sayfam | BurakDemir || PratikHack</title>
+    <link rel="stylesheet" href="ek/vendors/themify-icons/css/themify-icons.css">
+	<link rel="stylesheet" href="ek/css/portfolioph.css">
+    <link rel="icon" href="https://raw.githubusercontent.com/PratikHackTR/Resimler/master/logosiyah.ico" type="image/x-icon" />
+     <style>
+    .header-mono {
+      font-family: monospace;
+    }
+  </style>
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="40" id="home">
+    <header class="header">
+        <div class="container">
+            <ul class="social-icons pt-3">
+                <li class="social-item"><a class="social-link text-light" target="_blank" href="https://www.facebook.com/Bjk010203"><i class="ti-facebook icon-lg" aria-hidden="true"></i></a></li>
+                <li class="social-item"><a class="social-link text-light" target="_blank" href="https://www.x.com/burak_demir0"><i class="ti-twitter icon-lg" aria-hidden="true"></i></a></li>
+                <li class="social-item"><a class="social-link text-light" target="_blank" href="https://www.youtube.com/c/PratikHack01"><i class="ti-youtube icon-lg" aria-hidden="true"></i></a></li>
+                <li class="social-item"><a class="social-link text-light" target="_blank" href="https://www.instagram.com/burak_demirr01"><i class="ti-instagram icon-lg" aria-hidden="true"></i></a></li>
+                <li class="social-item"><a class="social-link text-light" target="_blank" href="https://www.github.com/PratikHackTR"><i class="ti-github icon-lg" aria-hidden="true"></i></a></li>
+            </ul>  
+            <div class="header-content">
+                <h4 class="header-subtitle" >Selam , ben</h4>
+                <h1 class="header-title">Burak Demir (PratikHack)</h1>
+                 <h6 class="header-mono" id="changing-text">Yazılım geliştiricisi</h6>
+            </div>
+        </div>
+    </header>
+    <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-white" data-spy="affix" data-offset-top="510">
+        <div class="container">
+            <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse mt-sm-20 navbar-collapse" id="navbarSupportedContent">
+             <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a href="#home" class="nav-link">AnaSayfa</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#about" class="nav-link">Hakkımda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="index.php#resume" class="nav-link">Özgeçmişim</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav brand">
+                    <img src="ek/resimler//avatar.gif" alt="" class="brand-img">
+                    <li class="brand-txt">
+                        <h5 class="brand-title">Burak Demir || PratikHack</h5>
+                        <div class="brand-subtitle">Web Designer | Developer | Network Master | </div>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ml-auto">
+
+                   
+                    <li class="nav-item last-item">
+                        <a href="#contact" class="nav-link">İletişim</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container-fluid" name="#about">
+        <div id="about" class="row about-section">
+            <div class="col-lg-4 about-card">
+                <h3 class="font-weight-light">Kimim ben ?</h3>
+                <span class="line mb-5"></span>
+                <h5 class="mb-3">Yazılım geliştiricisi , Web Geliştiricisi , Grafik Tasarımcı , Network yöneticisi , IT Uzmanı</h5>
+                <p class="mt-20">gibi gibi liste uzayıp gidiyor aşağıdan PDF uzantılı CV dosyamı indirip inceleyebilirsiniz</p>
+                <button class="btn btn-outline-danger"><i class="icon-down-circled2 "></i>CV indir</button>
+            </div>
+            <div class="col-lg-4 about-card">
+                <h3 class="font-weight-light">Kişisel bilgiler</h3>
+                <span class="line mb-5"></span>
+                <ul class="mt40 info list-unstyled">
+                    <li><span>Doğum Tarihi</span> : 14/10/2002</li>
+                    <li><span>E-posta</span> : burakdemirph@gmail.com</li>
+                    <li><span>Telefon</span> : + (90) 506 874 26 99</li>
+                    <li><span>Discord</span> : burakdemirph </li>
+                    <li><span>Address</span> :  Narlıca, 56073. Sk. No:48, 01180 Seyhan/Adana</li>
+                </ul>
+                <ul class="social-icons pt-3">
+                    <li class="social-item"><a class="social-link" href="#"><i class="ti-facebook icon-lg" aria-hidden="true"></i></a></li>
+                    <li class="social-item"><a class="social-link" href="#"><i class="ti-twitter icon-lg" aria-hidden="true"></i></a></li>
+                    <li class="social-item"><a class="social-link" href="#"><i class="ti-youtube icon-lg" aria-hidden="true"></i></a></li>
+                    <li class="social-item"><a class="social-link" href="#"><i class="ti-instagram icon-lg" aria-hidden="true"></i></a></li>
+                    <li class="social-item"><a class="social-link" href="#"><i class="ti-github icon-lg" aria-hidden="true"></i></a></li>
+                </ul>  
+            </div>
+            <div class="col-lg-4 about-card">
+                <h3 class="font-weight-light">Uzmanlık alanlarım</h3>
+                <span class="line mb-5"></span>
+                <div class="row">
+                    <div class="col-1 text-danger pt-1"><i class="ti-align-center icon-lg"></i></div>
+                    <div class="col-10 ml-auto mr-3">
+                        <h6>Uygulama & Yazılım geliştirme</h6>
+                        <p class="subtitle"> C# , C++ ve Python ağırlıklı olmak üzere</p>
+                        <hr>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-1 text-danger pt-1"><i class="ti-paint-bucket icon-lg"></i></div>
+                    <div class="col-10 ml-auto mr-3">
+                        <h6>Web programlama & Tasarım</h6>
+                        <p class="subtitle">Örnek olarak şu an içinde bulunduğunuz portfolyo sitem</p>
+                        <hr>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-1 text-danger pt-1"><i class="ti-stack-overflow icon-lg"></i></div>
+                    <div class="col-10 ml-auto mr-3">
+                        <h6>Veri tabanı yönetimi</h6>
+                        <p class="subtitle">MySQL , MSSQL , PostgreSQL , PL/SQL gibi veritabanları</p>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--Resume Section-->
+    <section class="section" id="resume">
+        <div class="container">
+            <h2 class="mb-5"><span class="text-danger">Öz</span> Geçmişim</h2>
+            <div class="row">
+                <div class="col-md-6 col-lg-4">
+                    <div class="card">
+                       <div class="card-header">
+                            <div class="mt-2">
+                                <h4>Deneyim</h4>
+                                <span class="line"></span>  
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <h6 class="title text-danger">2022 - Günümüz</h6>
+                            <P>IT Uzmanlığı , Network sunucu yönetimi& bakımı</P>
+                            <P class="subtitle">Başkent Üniversitesinin Araştırma hastanesinde Bilgi işlem olarak 1.5 yıldır süregelen çalışma hayatım.</P>
+                            <hr>
+                            <h6 class="title text-danger">2019 - 2020</h6>
+                            <P>Back&Front-end E.R.P JAVA Developer</P>
+                            <P class="subtitle">CanİAS. yazılımı başta olmak üzere ERP yazılımıyla birlikte şirket içi birkaç yazılım daha geliştirmiştim İmtek Mühendislik firması adı altında bilgi işlem biriminde. (aynı zamanda Staj yerimdir)</P>
+                            <hr>
+                            <h6 class="title text-danger">2014 - 2017</h6>
+                            <P>Freelance Yazılım geliştirme ve proje alma</P>
+                            <P class="subtitle">Çeşitli oyunlarda istek odaklı 3.parti yazılım geliştirme işlerinden ,restoran , stok takip yazılımları web geliştirme projeleri veya 
+                            (Bossa,otobansürücü kursu,çukurovabor,seyhanspor) gibi websiteleri</P> 
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <div class="card">
+                       <div class="card-header">
+                            <div class="mt-2">
+                                <h4>Eğitim</h4>
+                                <span class="line"></span>  
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <h6 class="title text-danger">2016 - 2020</h6>
+                            <P>Web Programcılığı mezunu</P>
+                            <P class="subtitle">İsmet İnönü Mesleki ve teknik anadolu lisesinden Bilişim bölümünde derece ile Web Programcılığı mezunuyum</P>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="card">
+                       <div class="card-header">
+                            <div class="pull-left">
+                                <h4 class="mt-2">Kısaca Bilgim</h4>
+                                <span class="line"></span>  
+                            </div>
+                        </div>
+                        <div class="card-body pb-2">
+                           <h6>PHP & HTML & CSS3 (WEB)</h6>
+                            <div class="progress mb-3">
+                                <div class="progress-bar bg-danger" role="progressbar" style="width: 97%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <h6>C# , C++ , Python</h6>
+                            <div class="progress mb-3">
+                                <div class="progress-bar bg-danger" role="progressbar" style="width: 85%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <h6>JavaScript</h6>
+                            <div class="progress mb-3">
+                                <div class="progress-bar bg-danger" role="progressbar" style="width: 80%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <h6>SQL</h6>
+                            <div class="progress mb-3">
+                                <div class="progress-bar bg-danger" role="progressbar" style="width: 90%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <h6>Grafik & Animasyon & Tasarım</h6>
+                            <div class="progress mb-3">
+                                <div class="progress-bar bg-danger" role="progressbar" style="width: 90%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <h6>Modelleme & Oyun tasarımı ve geliştirme</h6>
+                            <div class="progress mb-3">
+                                <div class="progress-bar bg-danger" role="progressbar" style="width: 90%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                       <div class="card-header">
+                            <div class="pull-left">
+                                <h4 class="mt-2">Diller</h4>
+                                <span class="line"></span>  
+                            </div>
+                        </div>
+                        <div class="card-body pb-2">
+                           <h6>English</h6>
+                            <div class="progress mb-3">
+                                <div class="progress-bar bg-danger" role="progressbar" style="width: 80%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <h6>Türkçe</h6>
+                            <div class="progress mb-3">
+                                <div class="progress-bar bg-danger" role="progressbar" style="width: 100%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    
+    <section class="section bg-dark py-5" name="#freelance">
+        <div class="container text-center">
+            <h2 class="text-light mb-5 font-weight-normal">FreeLance işlere açığım</h2>
+            <a class="btn bg-primary w-lg" href="#contact">İletişime geç</a>
+        </div>
+    </section>
+
+   
+
+    <div class="section contact" id="contact">
+        <div id="map" class="map">
+            
+        </div>
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-8">
+                    <div class="contact-form-card">
+                        <h4 class="contact-title">İletişim formu eklenecek</h4>
+                        <!--<form action="">
+                            <div class="form-group">
+                                <input  class="form-control" type="text" placeholder="İsminiz *" required>
+                            </div>
+                            <div class="form-group">
+                                <input class="form-control" type="email" placeholder="E-posta adresiniz *" required>
+                            </div>
+                            <div class="form-group">
+                                <textarea class="form-control" id="" placeholder="Mesajınız *" rows="7" required></textarea>
+                            </div>
+                            <div class="form-group ">
+                                <button type="submit" class="form-control btn btn-primary" >Send Message</button>
+                            </div>
+                        </form>-->
+                    </div> 
+                </div>
+                <div class="col-lg-4">
+                    <div class="contact-info-card">
+                        <h4 class="contact-title">İletişim bilgilerim</h4>
+                        <div class="row mb-2">
+                            <div class="col-1 pt-1 mr-1">
+                                <i class="ti-mobile icon-md"></i>
+                            </div>
+                            <div class="col-10 ">
+                                <h6 class="d-inline">Telefon : <br> <span class="text-muted">+ (90) 506-874-2699</span></h6>
+                            </div>
+                        </div>
+                        <div class="row mb-2">
+                            <div class="col-1 pt-1 mr-1">
+                                <i class="ti-map-alt icon-md"></i>
+                            </div>
+                            <div class="col-10">
+                                <h6 class="d-inline">Adres :<br> <span class="text-muted">56073. Sk. No:48Narlıca, 01180 Seyhan/Adana</span></h6>
+                            </div>
+                        </div>
+                        <div class="row mb-2">
+                            <div class="col-1 pt-1 mr-1">
+                                <i class="ti-envelope icon-md"></i>
+                            </div>
+                            <div class="col-10">
+                                <h6 class="d-inline">E-posta :<br> <span class="text-muted">burakdemirph@gmail.com</span></h6>
+                            </div>
+                        </div>
+                        <ul class="social-icons pt-4">
+                            <li class="social-item"><a class="social-link text-dark" target="_blank" href="https://www.facebook.com/Bjk010203"><i class="ti-facebook" aria-hidden="true"></i></a></li>
+                            <li class="social-item"><a class="social-link text-dark" target="_blank" href="https://www.x.com/burak_demir0"><i class="ti-twitter" aria-hidden="true"></i></a></li>
+                            <li class="social-item"><a class="social-link text-dark" target="_blank" href="https://www.yotuube.com/c/PratikHack01"><i class="ti-youtube" aria-hidden="true"></i></a></li>
+                            <li class="social-item"><a class="social-link text-dark" target="_blank" href="https://www.instagram.com/burak_demirr01"><i class="ti-instagram" aria-hidden="true"></i></a></li>
+                            <li class="social-item"><a class="social-link text-dark" target="_blank" href="https://www.github.com/PratikHackTR"><i class="ti-github" aria-hidden="true"></i></a></li>
+                        </ul> 
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <footer class="footer py-3">
+        <div class="container">
+            <p class="small mb-0 text-light">
+                &copy; <script>document.write(new Date().getFullYear())</script>  By PratikHack<a href="http://instagram.com/burak_demirr01" target="_blank"><span class="text-danger" title="Bootstrap 4 Themes and Dashboards"> Tıkla instagram'dan ulaş</span></a> 
+            </p>
+        </div>
+    </footer>
+
+	<!-- core  -->
+    <script src="ek/vendors/jquery/jquery-3.4.1.js"></script>
+    <script src="ek/vendors/bootstrap/bootstrap.bundle.js"></script>
+
+    <!-- bootstrap 3 affix -->
+    <script src="ek/vendors/bootstrap/bootstrap.affix.js"></script>
+
+    <!-- Isotope  -->
+    <script src="ek/vendors/isotope/isotope.pkgd.js"></script>
+    
+    <!-- Google mpas -->
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB8MrrsJYiGrCXkeWHkz6RghaotB4ko4FU&callback=initMap"></script>
+
+    <!-- portfolioph js -->
+    <script src="ek/js/portfolioph.js"></script>
+
+
+
+  <script>
+    var sentences = [
+      "Web Geliştiricisi",
+      "Grafik & tasarım",
+      "Network & Ağ yönetimi",
+      "SQL,C,C#,C++,ASP.net,Java,Python,JavaScript,PHP,HTML+CSS",
+      "İnovatif , gelişime açık , GENÇ"
+    ];
+
+    var index = 0;
+
+    function changeText() {
+      document.getElementById("changing-text").textContent = sentences[index];
+      index = (index + 1) % sentences.length;
+    }
+
+    setInterval(changeText, 5000); // 3000 milisaniye (3 saniye) aralıklarla metni değiştir
+  </script>
+</body>
+</html>
